908e4722dd0b8c5e44bc77da536adda2981a074c
Mauricio Anaya <46908589+mauanaya@users.noreply.github.com>
2020-01-30 09:22:12 -0600

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,9 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Repaso de python 3 y analisis de precios OHLC                              -- #
+# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
+# -- Rep: https://github.com/ITESOIF/MyST/tree/master/Notas_Python/Notas_RepasoPython     -- #
+# -- Autor: Francisco ME                                                                  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+OA_Ak = 'c' + 'da5127f163305921b7ac4fbeb513c08-25deeea97c3ca2fc2bac1d50093a7c6' + '8'
